b332c6fbeb2207ce57377a58c7f7b0a9d2aef4ff
mohammadreza memar <moreza.memar@gmail.com>
2023-06-01 15:41:50 -0500

    start adding logic to regiter a user

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect to MongoDB Atlas
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
